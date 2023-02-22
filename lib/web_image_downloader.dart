41eb9ed080170ffa6c7d08d6cc6462d2f94f177b
sivaprasadnk <sivaprasadnk123@gmail.com>
2023-02-22 22:11:57 +0530

    first commit

--- lib/web_image_downloader.dart
+++ lib/web_image_downloader.dart
@@ -1,7 +1,35 @@
-library web_image_downloader;
+import 'dart:convert';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class WebImageDownloader {
+  ///
+  ///
+  /// Function to download / save image locally in Flutter web
+  ///
+  /// [global key] parameter cannot be null. It is used to get the required image details, to save.
+  ///
+  /// [fileName] parameter cannot be null. It sets the name of the file name to be saved.
+
+  static Future<void> downloadImage(
+      GlobalKey globalKey, String fileName) async {
+    final RenderRepaintBoundary boundary =
+        globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage(pixelRatio: 3);
+
+    html.AnchorElement(href: await _imageToDataUrl(image))
+      ..setAttribute('download', fileName)
+      ..click();
+  }
+
+  static Future<String> _imageToDataUrl(ui.Image image) async {
+    final ByteData? byteData =
+        await image.toByteData(format: ui.ImageByteFormat.png);
+    return 'data:image/png;base64,${base64Encode(byteData!.buffer.asUint8List())}';
+  }
 }
